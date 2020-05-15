9d342f5cde3b585cc823d981a8a3790431fb2c68
Marcin Bonka <marcin.bonka@gmail.com>
2020-05-15 10:38:40 +0200

    Additiona log message in the main.js

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ var secondName='Krzysztof';
 var sureName='Bonka';
 
 console.log(`Hello, my full name is ${firstName} ${secondName} ${sureName}. What do you think?`);
+console.log(`Dodatkowe info tak dla przetestowania kolejnego commita`);

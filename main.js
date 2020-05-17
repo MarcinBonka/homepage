b39ed0fea643010e9bbe5e6b2548f2a2a2f242f6
Marcin Bonka <marcin.bonka@gmail.com>
2020-05-18 01:13:14 +0200

    Updated 'Moja praca zawodowa' page with new features learned based on lesson 04 of the training

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-var firstName='Marcin';
-var secondName='Krzysztof';
-var sureName='Bonka';
+var firstName = "Marcin";
+var secondName = "Krzysztof";
+var sureName = "Bonka";
 
-console.log(`Hello, my full name is ${firstName} ${secondName} ${sureName}. What do you think?`);
+console.log(
+  `Hello, my full name is ${firstName} ${secondName} ${sureName}. What do you think?`
+);
 console.log(`Dodatkowe info tak dla przetestowania kolejnego commita`);
